367dd92129efb951450799f3cf5464c26a5a9c9c
Tony Huang <t.tonyhuang@gmail.com>
2021-02-15 22:19:22 -0500

    add some mainpage formatting

--- lib/UI/widgets/logged_in_temp.dart
+++ lib/UI/widgets/logged_in_temp.dart
@@ -4,47 +4,66 @@ class LoggedInWidgetTemp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-          padding: EdgeInsets.only(left: 80, top: 50, right: 20),
+      backgroundColor: Color(0xFF1f213f),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            alignment: Alignment.topCenter,
+            image: AssetImage('assets/images/background.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: EdgeInsets.only(left: 20, top: 40, right: 25),
           child: Column(
             children: [
               // Top row with welcome and picture of logged in user
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                        height: 5,
-                      ),
-                      Text(
-                        'Welcome, ',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 30),
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      Text(
-                        'Tony',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 30),
-                      ),
-                    ],
+                  Padding(
+                    padding: EdgeInsets.only(top: 5, left: 70),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Text(
+                          'Welcome, ',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30,
+                              color: Colors.white),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          'Tony',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30,
+                              color: Colors.white),
+                        )
+                      ],
+                    ),
                   ),
                   CircleAvatar(
-                    radius: 25,
+                    radius: 20,
                     backgroundColor: Colors.white,
                     child: CircleAvatar(
-                      radius: 20,
-                      backgroundImage: NetworkImage('assets/images/0.jpg'),
+                      radius: 18,
+                      backgroundImage: AssetImage('assets/images/0.jpg'),
                     ),
-                  )
+                  ),
                 ],
               ),
             ],
-          )),
+          ),
+        ),
+      ),
     );
   }
 }

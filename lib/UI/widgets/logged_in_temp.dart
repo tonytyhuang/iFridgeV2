0cdcdffaac0bd03b150b707046e52f4f0a642673
Tony Huang <t.tonyhuang@gmail.com>
2021-02-16 17:35:02 -0500

    add scroll view

--- lib/UI/widgets/logged_in_temp.dart
+++ lib/UI/widgets/logged_in_temp.dart
@@ -8,148 +8,151 @@ class LoggedInWidgetTemp extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: Color(0xFF1f213f),
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            alignment: Alignment.topCenter,
-            image: AssetImage('assets/images/background.png'),
-            fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              alignment: Alignment.topCenter,
+              image: AssetImage('assets/images/background.png'),
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-        child: Padding(
-          padding: EdgeInsets.only(left: 10, top: 30, right: 10),
-          child: Column(
-            children: [
-              // Top row with welcome and picture of logged in user
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(top: 15, left: 70),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          height: 30,
-                        ),
-                        Text(
-                          'Welcome, ',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 30,
-                              color: Colors.white),
-                        ),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        Text(
-                          'Tony',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 30,
-                              color: Colors.white),
-                        )
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 10, right: 10),
-                    child: InkWell(
-                      onTap: () {
-                        Profile();
-                      },
-                      child: CircleAvatar(
-                        radius: 20,
-                        backgroundColor: Colors.white,
-                        child: CircleAvatar(
-                          radius: 18,
-                          backgroundImage: AssetImage('assets/images/0.jpg'),
-                        ),
+          child: Padding(
+            padding: EdgeInsets.only(left: 10, top: 30, right: 10),
+            child: Column(
+              children: [
+                // Top row with welcome and picture of logged in user
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: 15, left: 70),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            height: 30,
+                          ),
+                          Text(
+                            'Welcome, ',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30,
+                                color: Colors.white),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            'Tony',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30,
+                                color: Colors.white),
+                          )
+                        ],
                       ),
                     ),
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: 5, right: 5, top: 15),
-                child: Container(
-                  width: width,
-                  height: 220.0,
-                  color: Colors.transparent,
-                  child: Container(
-                    decoration: BoxDecoration(
-                        color: Color(0xFF233168),
-                        borderRadius: BorderRadius.circular(20)),
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding:
-                              EdgeInsets.only(left: 30, right: 30, top: 30),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                'Expiring Soon',
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 18),
-                              ),
-                              Text(
-                                'All Items >',
-                                style:
-                                    TextStyle(color: Colors.grey, fontSize: 13),
-                              )
-                            ],
+                    Padding(
+                      padding: EdgeInsets.only(top: 10, right: 10),
+                      child: InkWell(
+                        onTap: () {
+                          Profile();
+                        },
+                        child: CircleAvatar(
+                          radius: 20,
+                          backgroundColor: Colors.white,
+                          child: CircleAvatar(
+                            radius: 18,
+                            backgroundImage: AssetImage('assets/images/0.jpg'),
                           ),
                         ),
-                      ],
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 5, right: 5, top: 15),
+                  child: Container(
+                    width: width,
+                    height: 220.0,
+                    color: Colors.transparent,
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Color(0xFF233168),
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding:
+                                EdgeInsets.only(left: 30, right: 30, top: 30),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'Expiring Soon',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 18),
+                                ),
+                                Text(
+                                  'All Items >',
+                                  style: TextStyle(
+                                      color: Colors.grey, fontSize: 13),
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: 5, right: 5, top: 15),
-                child: Container(
-                  width: width,
-                  height: 240.0,
-                  color: Colors.transparent,
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding:
+                      EdgeInsets.only(left: 5, right: 5, top: 15, bottom: 20),
                   child: Container(
-                    decoration: BoxDecoration(
-                        color: Color(0xFF233168),
-                        borderRadius: BorderRadius.circular(20)),
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding:
-                              EdgeInsets.only(left: 30, right: 30, top: 30),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                'Recipes To Use',
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 18),
-                              ),
-                              Text(
-                                'All Recipes >',
-                                style:
-                                    TextStyle(color: Colors.grey, fontSize: 13),
-                              )
-                            ],
+                    width: width,
+                    height: 350.0,
+                    color: Colors.transparent,
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Color(0xFF233168),
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding:
+                                EdgeInsets.only(left: 30, right: 30, top: 30),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'Recipes To Use',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 18),
+                                ),
+                                Text(
+                                  'All Recipes >',
+                                  style: TextStyle(
+                                      color: Colors.grey, fontSize: 13),
+                                )
+                              ],
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),

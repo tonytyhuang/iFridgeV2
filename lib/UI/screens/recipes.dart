2abca47e87bb70d25ff3e7ad2526eba911aba2e4
Tony Huang <t.tonyhuang@gmail.com>
2021-03-08 23:04:54 -0500

    finish recipe list

--- lib/UI/screens/recipes.dart
+++ lib/UI/screens/recipes.dart
@@ -35,14 +35,78 @@ class _RecipesState extends State<Recipes> {
           builder: (context, snapshot) {
             if (snapshot.hasData) {
               return ListView.builder(
-                  itemCount: snapshot.data.length,
-                  itemBuilder: (context, index) {
-                    var item = snapshot.data[index];
-                    return Container(
-                      height: 100,
-                      child: Image.network(item.image),
-                    );
-                  });
+                itemCount: snapshot.data.length,
+                itemBuilder: (context, index) {
+                  var item = snapshot.data[index];
+                  return Container(
+                    height: 133,
+                    child: Card(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding:
+                                EdgeInsets.only(top: 10, bottom: 10, left: 10),
+                            child: Image.network(item.image),
+                          ),
+                          Column(
+                            //mainAxisAlignment: MainAxisAlignment.end,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Flexible(
+                                child: Padding(
+                                  padding: EdgeInsets.only(
+                                      top: 10, bottom: 0, left: 15, right: 15),
+                                  child: Text(
+                                    item.title,
+                                    maxLines: 2,
+                                    softWrap: true,
+                                    overflow: TextOverflow.clip,
+                                    style: TextStyle(
+                                        color: Colors.black, fontSize: 15),
+                                  ),
+                                ),
+                              ),
+                              Spacer(),
+                              Padding(
+                                padding: EdgeInsets.only(
+                                    top: 5, bottom: 0, left: 15, right: 15),
+                                child: Text(
+                                  'Used Ingredients: ' +
+                                      item.usedIngredientCount.toString(),
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 10),
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(
+                                    top: 5, bottom: 0, left: 15, right: 15),
+                                child: Text(
+                                  'Unused Ingredients: ' +
+                                      item.missedIngredientCount.toString(),
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 10),
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(
+                                    top: 5, bottom: 0, left: 15, right: 15),
+                                child: Text(
+                                  'Likes: ' + item.likes.toString(),
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 10),
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                      // child: Image.network(item.image),
+                    ),
+                  );
+                },
+              );
             } else if (snapshot.hasError) {
               return Text("${snapshot.error}");
             }
--- lib/api/api_urls.dart
+++ lib/api/api_urls.dart
@@ -1,6 +1,6 @@
 class Url {
   static String url =
-      "https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=3&apiKey=f4fd5002222b4e9eb0f35956381904f0";
+      "https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=10&apiKey=f4fd5002222b4e9eb0f35956381904f0";
   static String setUrl(List ingredients) {
     String url =
         "https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=10&apiKey=f4fd5002222b4e9eb0f35956381904f0";
